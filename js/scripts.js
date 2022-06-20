4da7766cbec4198af6a6f5992a82baade69d7e3f
akemo issa <akemoissa@gmail.com>
2022-06-20 17:41:57 +0300

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+/*!
+* Start Bootstrap - Blog Home v5.0.8 (https://startbootstrap.com/template/blog-home)
+* Copyright 2013-2022 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-blog-home/blob/master/LICENSE)
+*/
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
+
+function myFunction() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more"; 
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less"; 
+    moreText.style.display = "inline";
+  }
+}
